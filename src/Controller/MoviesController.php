855e9a0f48d394ad41448fc8837d4aa9998dd1bf
erikonasz <arcikauskas.erikas@gmail.com>
2023-12-07 16:37:06 +0200

    practise with blocks

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,18 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies/{name}', name: 'movies', defaults:['name' => null], methods:['GET', 'HEAD'])]
-    public function index($name): JsonResponse
+    #[Route('/movies', name: 'movies')]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => $name,
-            'path' => 'src/Controller/MoviesController.php',
-        ]);
+        $movies = ["Avengers: Endgame", "inception","Loki","Black Widow"];
+
+        return $this->render('index.html.twig', array(
+            'movies' => $movies
+        ));
     }
 
 }
